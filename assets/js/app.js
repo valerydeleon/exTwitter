4b962c72312e090d7d2637585ea13c63bc5f7947
Valeria de Leon <valery.deleon@gmail.com>
2017-04-04 00:01:54 -0500

    cuenta caracteres

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,7 @@
-var contador = 0;
 var textoDelTextArea = document.getElementById('inputTextArea');
 var textoNombreAutorTweet = document.getElementById('inputNombreAutor');
+var contadorTweetsClick = document.getElementById("contadorTweets");
+var contadorTweets = 0;
 
 function escribirTweet(){
   var contenedorDeTweets = document.getElementById('contenedorDeTweets');
@@ -9,13 +10,17 @@ function escribirTweet(){
   var autorTweet = document.createElement("p");
   var division = document.createElement("hr");
 
-
   tweets.innerText = textoDelTextArea.value;
   textoDelTextArea.value = ""
+  //tweets.width = "100px"
 
-  autorTweet.innerText = textoNombreAutorTweet.value;
+  autorTweet.innerText = "Creado por: " + textoNombreAutorTweet.value;
   textoNombreAutorTweet.value = "";
+  // autorTweet.width = "100px";
+  autorTweet.align = "right";
+  // autorTweet.innerText = "Creado por: " +
 
+  //division.width = "300px";
 
   contenedorDeTweets.appendChild(tweets);
   tweets.appendChild(autorTweet);
@@ -23,5 +28,12 @@ function escribirTweet(){
 }
 
 function contadorDeLetras(){
+  //
+  //var contenedorCaracteres = document.getElementById("caracteres");
+  document.forms[0].caracteres.value = document.forms[0].texto.value.length;
+  //document.forms[0].caracteres.value = document.forms[0].texto.value.length
+}
 
+function contadorClicks(){
+  
 }
